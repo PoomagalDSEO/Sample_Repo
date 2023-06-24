fb53b9c4d86188cab1143d7ef618232d1aec94d5
PoomagalDSEO <137484031+PoomagalDSEO@users.noreply.github.com>
2023-06-24 16:59:26 +0530

    Update sample_app.py

--- sample_app.py
+++ sample_app.py
@@ -3,17 +3,23 @@ import streamlit as st
 # @st.cache_data #cache data within app to reuse the same data again
 # Main Streamlit app code
 def main():
-    AUDIDENCE_TPYE = st.selectbox('AUDIDENCE_TYPE', ['Beginner', 'Intermediate', 'Expert'])
+    # AUDIDENCE_TPYE = st.selectbox('AUDIDENCE_TYPE', ['Beginner', 'Intermediate', 'Expert'])
     STRUCTURE_TPYES = ['Thematic', 'Problem-Solution', 'Chronological', 'Cause-Effect']
     NARRATIVE_TPYES = ['Factual', 'Persuasive', 'Anecdotal', 'Descriptive']
+
+    AUDIDENCE_TPYE = st.selectbox('AUDIDENCE_TYPE', ['Beginner', 'Intermediate', 'Expert'])
+
     if AUDIDENCE_TPYE == 'Beginner':
         STRUCTURE_TPYE = ['Thematic', 'Problem-Solution']
+        # NARRATIVE_TPYE = ['Descriptive']
         # return ['Descriptive']
     elif AUDIDENCE_TPYE == 'Intermediate':
         STRUCTURE_TPYE = ['Problem-Solution', 'Chronological']
+        # NARRATIVE_TPYE = ['Persuasive']
         # return ['Factual']
     elif AUDIDENCE_TPYE == 'Expert':
         STRUCTURE_TPYE = ['Chronological', 'Cause-Effect']
+        # NARRATIVE_TPYE = ['Persuasive']
         # return ['Persuasive']
     else:
         STRUCTURE_TPYE = []
@@ -21,17 +27,19 @@ def main():
     STRUCTURE_TPYE = st.selectbox('STRUCTURE TYPE', STRUCTURE_TPYE)
 
     if STRUCTURE_TPYE == 'Thematic':
-        return ['Descriptive']
+        NARRATIVE_TPYE = 'Descriptive'
     elif STRUCTURE_TPYE == 'Problem-Solution':
-        NARRATIVE_TPYE = ['Factual']
+        NARRATIVE_TPYE = 'Factual'
     elif STRUCTURE_TPYE == 'Chronological':
-        NARRATIVE_TPYE = ['Persuasive']
+        NARRATIVE_TPYE = 'Persuasive'
     elif STRUCTURE_TPYE == 'Cause-Effect':
-        NARRATIVE_TPYE = ['Anecdotal']
+        NARRATIVE_TPYE = 'Anecdotal'
     else:
         NARRATIVE_TPYE = []
 
-    st.write(f"NARRATIVE TYPE IS:   {NARRATIVE_TPYE}")
-
+    # st.write(f"NARRATIVE TYPE IS:   {NARRATIVE_TPYE}")
+    st.code(f"AUDIENCE_TPYES = {AUDIDENCE_TPYE}")
+    st.code(f"STRUCTURE_TPYES = {STRUCTURE_TPYE}")
+    st.code(f"NARRATIVE_TPYES = {NARRATIVE_TPYE}")
 if __name__ == '__main__':
     main()
