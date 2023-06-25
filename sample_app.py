f003e5567d9abb78761493d8b67ce12497611d66
PoomagalDSEO <137484031+PoomagalDSEO@users.noreply.github.com>
2023-06-25 16:36:35 +0530

    Update sample_app.py

--- sample_app.py
+++ sample_app.py
@@ -10,7 +10,7 @@ def main():
     AUDIDENCE_TPYE = st.selectbox('AUDIDENCE_TYPE', ['Beginner', 'Intermediate', 'Expert'])
 
     if AUDIDENCE_TPYE == 'Beginner':
-        STRUCTURE_TPYE = ['Thematic', 'Problem-Solution']
+        STRUCTURE_TPYES = ['Thematic', 'Problem-Solution']
         # NARRATIVE_TPYE = ['Descriptive']
         # return ['Descriptive']
     elif AUDIDENCE_TPYE == 'Intermediate':
@@ -18,34 +18,37 @@ def main():
         # NARRATIVE_TPYE = ['Persuasive']
         # return ['Factual']
     elif AUDIDENCE_TPYE == 'Expert':
-        STRUCTURE_TPYE = ['Chronological', 'Cause-Effect']
+        STRUCTURE_TPYES = ['Chronological', 'Cause-Effect']
         # NARRATIVE_TPYE = ['Persuasive']
         # return ['Persuasive']
     else:
-        STRUCTURE_TPYE = []
+        STRUCTURE_TPYES = []
 
-    STRUCTURE_TPYE = st.selectbox('STRUCTURE TYPE', STRUCTURE_TPYE)
+    STRUCTURE_TPYE = st.selectbox('STRUCTURE TYPE', STRUCTURE_TPYES)
 
     if STRUCTURE_TPYE == 'Thematic':
-        NARRATIVE_TPYE = 'Descriptive'
+        NARRATIVE_TPYES = ['Descriptive']
     elif STRUCTURE_TPYE == 'Problem-Solution':
-        NARRATIVE_TPYE = 'Factual'
+        NARRATIVE_TPYES = ['Factual']
     elif STRUCTURE_TPYE == 'Chronological':
-        NARRATIVE_TPYE = 'Persuasive'
+        NARRATIVE_TPYES = ['Persuasive']
     elif STRUCTURE_TPYE == 'Cause-Effect':
-        NARRATIVE_TPYE = 'Anecdotal'
+        NARRATIVE_TPYES = ['Anecdotal']
     else:
-        NARRATIVE_TPYE = []
+        NARRATIVE_TPYES = []
+
+    NARRATIVE_TPYE = st.selectbox('NARRATIVE TYPE', NARRATIVE_TPYES)
 
     # st.write(f"NARRATIVE TYPE IS:   {NARRATIVE_TPYE}")
-    st.code(f"AUDIENCE_TPYES = {AUDIDENCE_TPYE}")
-    st.code(f"STRUCTURE_TPYES = {STRUCTURE_TPYE}")
-    st.code(f"NARRATIVE_TPYES = {NARRATIVE_TPYE}")
+
+    st.code(f"AUDIENCE_TPYES = [{AUDIDENCE_TPYE}]")
+    st.code(f"STRUCTURE_TPYES = [{STRUCTURE_TPYE}]")
+    st.code(f"NARRATIVE_TPYES = {NARRATIVE_TPYES}")
+
+if __name__ == '__main__':
+    main()
 
 uploaded_file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
 
 if uploaded_file is not None:
-        st.image(uploaded_file, caption='Uploaded Image')
-
-if __name__ == '__main__':
-    main()
+   st.image(uploaded_file, caption='Uploaded Image')
