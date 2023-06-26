db089c89673df14bf7d89af3a7e0029f6bf75aeb
PoomagalDSEO <137484031+PoomagalDSEO@users.noreply.github.com>
2023-06-26 21:05:44 +0530

    Update sample_app.py

--- sample_app.py
+++ sample_app.py
@@ -1,54 +1,56 @@
+import whois
+import validators
+import gspread as gs
+import pandas as pd
 import streamlit as st
+from datetime import datetime, timedelta
 
-# @st.cache_data #cache data within app to reuse the same data again
-# Main Streamlit app code
+# Function to check domain expiry date
+def check_domain_expiry(dm):
+    if validators.domain(dm):
+        try:
+            dm_info = whois.whois(dm)
+            # return domain_info
+            exp_date = dm_info.expiration_date
+
+            # If expiry date is a list, consider the first element
+            if isinstance(exp_date, list):
+                exp_date = exp_date[0]
+
+            # Compare with current date
+            curr_date = datetime.now()
+            days_remaining = (exp_date - curr_date).days
+
+            if days_remaining <= 10:
+                return f"Domain {dm} is expiring soon. Expires in {days_remaining} days."
+            else:
+                return f"Domain {dm} is not expiring soon. Expires in {days_remaining} days."
+
+        except Exception as error:
+            return f"Error: {str(error)}"
+    # else:
+    #     return None
+            # f"Enter a valid domain"
+
+# Main function to get user input and check domain expiry
 def main():
-    # AUDIDENCE_TPYE = st.selectbox('AUDIDENCE_TYPE', ['Beginner', 'Intermediate', 'Expert'])
-    STRUCTURE_TPYES = ['Thematic', 'Problem-Solution', 'Chronological', 'Cause-Effect']
-    NARRATIVE_TPYES = ['Factual', 'Persuasive', 'Anecdotal', 'Descriptive']
+    gc = gs.service_account(filename='connections.json')
+    sh = gc.open_by_url(
+        'https://docs.google.com/spreadsheets/d/15mODRXCtY1MoC9ChfUamm4IGmwTNuOpLK7TpYkM_fNQ/edit#gid=503088821')
+    ws = sh.worksheet('sheet1')
+    df = pd.DataFrame(ws.get_all_records())
+    df2= df[df.columns[0]].values.tolist()
+    print(df2)
+    DM = df2
+    for dm in DM:
+        print(dm)
+        if dm != '':
+            result = check_domain_expiry(dm)
+            st.write(result)
+            # print(result)
+        else:
 
-    AUDIDENCE_TPYE = st.selectbox('AUDIDENCE_TYPE', ['Beginner', 'Intermediate', 'Expert'])
-
-    if AUDIDENCE_TPYE == 'Beginner':
-        STRUCTURE_TPYES = ['Thematic', 'Problem-Solution']
-        # NARRATIVE_TPYE = ['Descriptive']
-        # return ['Descriptive']
-    elif AUDIDENCE_TPYE == 'Intermediate':
-        STRUCTURE_TPYE = ['Problem-Solution', 'Chronological']
-        # NARRATIVE_TPYE = ['Persuasive']
-        # return ['Factual']
-    elif AUDIDENCE_TPYE == 'Expert':
-        STRUCTURE_TPYES = ['Chronological', 'Cause-Effect']
-        # NARRATIVE_TPYE = ['Persuasive']
-        # return ['Persuasive']
-    else:
-        STRUCTURE_TPYES = []
-
-    STRUCTURE_TPYE = st.selectbox('STRUCTURE TYPE', STRUCTURE_TPYES)
-
-    if STRUCTURE_TPYE == 'Thematic':
-        NARRATIVE_TPYES = ['Descriptive']
-    elif STRUCTURE_TPYE == 'Problem-Solution':
-        NARRATIVE_TPYES = ['Factual']
-    elif STRUCTURE_TPYE == 'Chronological':
-        NARRATIVE_TPYES = ['Persuasive']
-    elif STRUCTURE_TPYE == 'Cause-Effect':
-        NARRATIVE_TPYES = ['Anecdotal']
-    else:
-        NARRATIVE_TPYES = []
-
-    NARRATIVE_TPYE = st.selectbox('NARRATIVE TYPE', NARRATIVE_TPYES)
-
-    # st.write(f"NARRATIVE TYPE IS:   {NARRATIVE_TPYE}")
-
-    st.code(f"AUDIENCE_TPYES = [{AUDIDENCE_TPYE}]")
-    st.code(f"STRUCTURE_TPYES = [{STRUCTURE_TPYE}]")
-    st.code(f"NARRATIVE_TPYES = {NARRATIVE_TPYES}")
-
-if __name__ == '__main__':
+            break
+            # print('No element')
+if __name__ == "__main__":
     main()
-
-uploaded_file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
-
-if uploaded_file is not None:
-   st.image(uploaded_file, caption='Uploaded Image')
